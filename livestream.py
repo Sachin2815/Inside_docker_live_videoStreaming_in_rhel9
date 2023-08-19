33dab5d78461534aa092769973051c1d6e390d32
Sachin Singh <100368589+Sachin2815@users.noreply.github.com>
2023-08-19 20:48:38 +0530

    Update livestream.py

--- livestream.py
+++ livestream.py
@@ -4,7 +4,7 @@ cap = cv2.VideoCapture(0)
 
 while True:
     status, photo = cap.read()
-    cv2.imshow("Mayank's Webcam Video Stream", photo)
+    cv2.imshow("Sachin's:wq Webcam Video Stream", photo)
 
     # Press Enter key to exit
     if cv2.waitKey(10) == 13:
